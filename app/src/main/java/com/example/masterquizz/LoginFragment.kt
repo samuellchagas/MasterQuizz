c8428139c97a12352ca18fd1ce9aad91ec0d984a
samuell <samuelltaylor@gmail.com>
2022-04-15 20:02:58 -0300

    Criação da tela de seleção de materia

--- app/src/main/java/com/example/masterquizz/LoginFragment.kt
+++ app/src/main/java/com/example/masterquizz/LoginFragment.kt
@@ -1,4 +1,42 @@
 package com.example.masterquizz
 
-class LoginFragment {
+import android.graphics.Color
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.graphics.drawable.DrawableCompat.inflate
+
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.masterquizz.databinding.ActivityMainBinding.inflate
+import com.example.masterquizz.databinding.FragmentLoginBinding
+
+
+class LoginFragment: Fragment() {
+
+    private val binding: FragmentLoginBinding by lazy {
+        FragmentLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        Handler(Looper.getMainLooper()).postDelayed(
+//            {
+//                findNavController().navigate(R.id.NOME DA NAVEGAÇÃO)
+//            }, SPLASH_DELAY
+//        )
+    }
+
 }
--- app/src/main/java/com/example/masterquizz/SubjectFragment.kt
+++ app/src/main/java/com/example/masterquizz/SubjectFragment.kt
@@ -1,4 +1,28 @@
 package com.example.masterquizz
 
-class SubjectFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.masterquizz.databinding.FragmentLoginBinding
+import com.example.masterquizz.databinding.FragmentSubjectsBinding
+
+class SubjectFragment: Fragment() {
+
+    private val binding: FragmentSubjectsBinding by lazy {
+        FragmentSubjectsBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
